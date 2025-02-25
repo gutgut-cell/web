75376ba350770714f1a39a5f6985b33f5d95a6a8
gutgut-cell <anshusuresh121@gmail.com>
2025-02-25 06:24:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -135,4 +135,6 @@ def home():
     return render_template("index.html", image_url=display_url, poem=poem)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a dynamic port
+    app.run(host="0.0.0.0", port=port)
+
