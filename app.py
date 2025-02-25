e92df484a5f96d0e2caf3f34a6255080aa78f700
gutgut-cell <anshusuresh121@gmail.com>
2025-02-25 06:27:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,18 @@ load_dotenv()
 app = Flask(__name__)
 
 # Google Drive API Credentials
-SERVICE_ACCOUNT_FILE = "website-451921-f977a4699285.json"
+
 FOLDER_ID = "1GcQ6lADVSPWaHnPh8oIFV1zJ7i88mSp4"
 
-SCOPES = ["https://www.googleapis.com/auth/drive"]
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES
-)
+import json
+
+credentials_json = os.getenv("GOOGLE_CREDENTIALS")  # Load from env variable
+if credentials_json:
+    credentials_dict = json.loads(credentials_json)
+    credentials = service_account.Credentials.from_service_account_info(credentials_dict, scopes=SCOPES)
+else:
+    raise ValueError("Google Credentials are missing! Set GOOGLE_CREDENTIALS in env variables.")
+
 
 def fetch_google_drive_images(folder_id):
     """Fetches both display and processing URLs from Google Drive."""
